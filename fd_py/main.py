7daca767c835d16eff66686f1b82654745722538
Wilson Oh <e0773510@u.nus.edu>
2023-01-09 23:55:47 +0800

    added support for CLA using argparse library; fd_py now supports filtering by hidden files, file type, file extension and regex patterns

--- fd_py/main.py
+++ fd_py/main.py
@@ -3,10 +3,12 @@ from collections.abc import Callable
 from pathlib import Path
 import re
 import sys
-from typing import Literal
+from typing import ClassVar, Literal
 from rich.tree import Tree
 from rich.text import Text
 from rich import print
+from argparse import ArgumentParser
+from dataclasses import dataclass
 
 
 class PathFilter(ABC):
@@ -15,29 +17,34 @@ class PathFilter(ABC):
         ...
 
 
+@dataclass
 class HiddenFilter(PathFilter):
     def filter(self, path: Path) -> bool:
         return path.name.startswith(".")
 
+    def __repr__(self) -> str:
+        return "HiddenFilter"
 
+
+@dataclass
 class FileExtensionFilter(PathFilter):
-    def __init__(self, file_extension: str) -> None:
-        self.file_extension = file_extension
+    file_extension: str
 
     def filter(self, path: Path) -> bool:
-        return path.is_file() and path.suffix.lstrip(".") == self.file_extension
+        return path.is_dir() or path.suffix.lstrip(".") == self.file_extension
 
 
+@dataclass
 class RegexFilter(PathFilter):
-    def __init__(self, pattern: str) -> None:
-        self.pattern = pattern
+    pattern: str
 
     def filter(self, path: Path) -> bool:
         return bool(re.findall(self.pattern, str(path.absolute())))
 
 
+@dataclass
 class FiletypeFilter(PathFilter):
-    filetype_map: dict[str, Callable[[Path], bool]] = {
+    filetype_map: ClassVar[dict[str, Callable[[Path], bool]]] = {
         "s": Path.is_socket,
         "f": Path.is_file,
         "d": Path.is_dir,
@@ -45,16 +52,15 @@ class FiletypeFilter(PathFilter):
         "l": Path.is_symlink,
     }
 
-    def __init__(self, filetype: Literal["s", "x", "f", "d", "p"]) -> None:
-        self.filetype = filetype
+    filetype: Literal["s", "x", "f", "d", "p"]
 
     def filter(self, path: Path) -> bool:
         return self.filetype_map[self.filetype](path)
 
 
+@dataclass
 class InverseFilter(PathFilter):
-    def __init__(self, pfilter: PathFilter) -> None:
-        self.pfilter = pfilter
+    pfilter: PathFilter
 
     def filter(self, path: Path) -> bool:
         return not self.pfilter.filter(path)
@@ -64,24 +70,82 @@ def ls(
     path: Path = Path("."),
     *,
     max_depth: int = 10,
-    filters: list[PathFilter] | None = None,
+    filters: list[PathFilter],
 ) -> Tree:
-    tree = Tree(Text(path.name, style="blue"))
+    tree = Tree(Text(path.name, style="blue"), guide_style="magenta")
     try:
         for file in Path(path).expanduser().iterdir():
-            if file.is_dir() and max_depth > 0:
-                tree.add(ls(file, max_depth=max_depth - 1, filters=filters))
-            if filters is not None and all(pf.filter(file) for pf in filters):
-                tree.add(file.name)
+            if all(pf.filter(file) for pf in filters):
+                if file.is_dir() and max_depth > 0:
+                    tree.add(ls(file, max_depth=max_depth - 1, filters=filters))
+                else:
+                    tree.add(Text(file.name, style=("blue" if file.is_dir() else "")))
     except PermissionError:
         print(f"access denied for file: {path}", file=sys.stderr)
     return tree
 
 
-filters = []
-filters.append(FiletypeFilter("f"))
-# filters.append(FileExtensionFilter("py"))
-# filters.append(FileExtensionFilter("pyc"))
-# filters.append(InverseFilter(RegexFilter("lib")))
-t = ls(Path("/etc"), max_depth=3, filters=filters)
-print(t)
+def main():
+    parser = ArgumentParser(prog="fd_py", description="fd but written in python")
+    parser.add_argument("path", default=".", nargs="?")
+    parser.add_argument(
+        "-H",
+        "--hidden",
+        action="store_false",
+        help="show hidden files as well",
+        default=True,
+    )
+
+    parser.add_argument(
+        "-t",
+        "--type",
+        choices=("s", "f", "d", "p"),
+        action="extend",
+        help="filter the type of file shown",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--pattern",
+        help="list files that match the pattern",
+        nargs="?",
+        default=".*",
+    )
+
+    parser.add_argument(
+        "-E",
+        "--exclude",
+        help="exclude files that match the pattern",
+        action="append",
+    )
+    parser.add_argument(
+        "--max-depth",
+        help="maximum depth of recursion when listing files",
+        type=int,
+        nargs="?",
+        default=10,
+    )
+    parser.add_argument(
+        "-e", "--ext", help="only show files with the extension", action="append"
+    )
+    args = parser.parse_args()
+    filters = []
+    if args.hidden:
+        filters.append(InverseFilter(HiddenFilter()))
+    if args.type:
+        for t in args.type:
+            filters.append(FiletypeFilter(t))
+    if args.pattern:
+        filters.append(RegexFilter(args.pattern))
+    if args.exclude:
+        for e in args.exclude:
+            filters.append(InverseFilter(RegexFilter(e)))
+    if args.ext:
+        for ext in args.ext:
+            filters.append(FileExtensionFilter(ext))
+    t = ls(Path(args.path), max_depth=args.max_depth, filters=filters)
+    print(t)
+
+
+if __name__ == "__main__":
+    main()
